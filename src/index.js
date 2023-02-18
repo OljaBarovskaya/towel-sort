9cc392d4b216d7f499e25bc6118b7e106c13dca5
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-02-18 10:29:45 +0300

    towel-sort done

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@
 
 module.exports = function towelSort (matrix) {
 let towel = [];
-for(let i=0; i<matrix.length; i++){
-    if (i % 2 === 0) {
-        for ( let j=0; j<matrix[i].length; j++) {
-         towel.push(matrix[i][j]);   
-        } 
-      } else {
-        matrix[i].reverse();
-        for ( let j=0; j<matrix[i].length; j++) {
-            towel.push(matrix[i][j]);   
-           } 
-      }
-}
+if (matrix === undefined) {return towel} else {
+    for(let i=0; i<matrix.length; i++){
+        if (i % 2 === 0) {
+            for ( let j=0; j<matrix[i].length; j++) {
+             towel.push(matrix[i][j]);   
+            } 
+          } else {
+            matrix[i].reverse();
+            for ( let j=0; j<matrix[i].length; j++) {
+                towel.push(matrix[i][j]);   
+               } 
+          }
+    }
+} 
+
 
   return towel;
 }
